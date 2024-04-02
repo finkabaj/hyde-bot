26da4974f10acaa2849e551124a91804888095c5
finkabaj <yuriy9505@gmail.com>
2024-04-02 23:24:28 +0400

    refactor: idiomatic func in db

--- internals/db/postgresql/postgresql.go
+++ internals/db/postgresql/postgresql.go
@@ -13,27 +13,19 @@ type Postgresql struct {
 	Conn *pgx.Conn
 }
 
-func (p *Postgresql) Connect(credentials *db.DatabaseCredentials) error {
+func (p *Postgresql) Connect(credentials *db.DatabaseCredentials) (err error) {
 	connStr := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable", credentials.User, credentials.Password, credentials.Host, credentials.Port, credentials.Database)
-	conn, err := pgx.Connect(context.Background(), connStr)
+	p.Conn, err = pgx.Connect(context.Background(), connStr)
 
-	if err != nil {
-		return err
-	}
-
-	p.Conn = conn
-
-	return nil
+	return
 }
 
 func (p *Postgresql) Close() {
 	p.Conn.Close(context.Background())
 }
 
-func (p *Postgresql) Status() error {
-	if err := p.Conn.Ping(context.Background()); err != nil {
-		return err
-	}
+func (p *Postgresql) Status() (err error) {
+	err = p.Conn.Ping(context.Background())
 
-	return nil
+	return
 }
