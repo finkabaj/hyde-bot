0563cd1d9173853213ef9d6a7175bf767bcde6a2
finkabaj <yuriy9505@gmail.com>
2024-04-03 16:15:16 +0400

    feat: add setup pg core

--- internals/db/postgresql/postgresql.go
+++ internals/db/postgresql/postgresql.go
@@ -13,6 +13,30 @@ type Postgresql struct {
 	Conn *pgx.Conn
 }
 
+func (p *Postgresql) setup() (err error) {
+	if err = p.Status(); err != nil {
+		return
+	}
+
+	ctx := context.Background()
+
+	transaction, err := p.Conn.Begin(ctx)
+
+	if err != nil {
+		return
+	}
+
+	defer func() {
+		if err != nil {
+			transaction.Rollback(ctx)
+		}
+	}()
+
+	err = transaction.Commit(ctx)
+
+	return
+}
+
 func (p *Postgresql) Connect(credentials *db.DatabaseCredentials) (err error) {
 	connStr := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable", credentials.User, credentials.Password, credentials.Host, credentials.Port, credentials.Database)
 	p.Conn, err = pgx.Connect(context.Background(), connStr)
